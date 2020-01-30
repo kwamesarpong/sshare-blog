8ffe45ef1bc6e186b0f4b12b1db620ca30db40fa
Francis Badasu <nytro04@gmail.com>
2020-01-30 10:24:09 +0000

    redux setup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
 
+import store from "./store";
 import Header from "./components/pages/Header";
 import "./scss/main.scss";
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <Provider store={store}>
         <BrowserRouter>
           <Switch>
             <Route exact path="/" component={Header} />
           </Switch>
         </BrowserRouter>
-      </div>
+      </Provider>
     );
   }
 }
