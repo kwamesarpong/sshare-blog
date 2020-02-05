c347058c59b2252a2d767548e0105bf4e879bd5c
Francis Badasu <nytro04@gmail.com>
2020-02-05 11:11:20 +0000

    first contribute page and mobile responsive completed

--- src/components/pages/Contribute.js
+++ src/components/pages/Contribute.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
 import Navbar from "./Navbar";
+import ContributeCard from "../utils/ContributeCard";
 
 // import peopleFlowerImg from "../../assets/contribute-flower.jpg";
 // import peopleFlowerImg1 from "../../assets/contribute-2.jpg";
 
 import { ReactComponent as ContributeImg1 } from "../../assets/SISTAZSHARE-CONTRIBUTE.svg";
 import { ReactComponent as ContributeImg2 } from "../../assets/SISTAZSHARE-CONTRIBUTE-2.svg";
-import ContributeCard from "../utils/ContributeCard";
+import { ReactComponent as ContributeBadge } from "../../assets/SISTAZSHARE-BADGE.svg";
+import { ReactComponent as ContributeImg3 } from "../../assets/SISTAZSHARE-CONTRIBUTE-3.svg";
 
 class Contribute extends Component {
   state = {
@@ -37,7 +41,7 @@ class Contribute extends Component {
       <div className="contribute">
         <Navbar whitePage={true} />
 
-        <div className="container">
+        <div className="container pb-5">
           <div className="row">
             <div className="col-md-6">
               {/* // svg image  */}
@@ -71,34 +75,81 @@ class Contribute extends Component {
           </div>
         </div>
 
-        <div className="container pt-5">
+        <hr />
+
+        <div className="container">
           <h3 className="heading heading__secondary-2 pt-5 text-center">
             AllerVite !
           </h3>
-
           <div className="contribute__section-2 pb-5">
             <div className="row justify-content-center">
               <div className="col-md-6">
                 <ContributeImg2 className="img-fluid" />
               </div>
             </div>
+
+            <div className="row pb-5">
+              <ContributeCard
+                num={this.state.num.num1}
+                title={this.state.title.title1}
+                text={this.state.text.text1}
+              />
+              <ContributeCard
+                num={this.state.num.num2}
+                title={this.state.title.title2}
+                text={this.state.text.text2}
+              />
+              <ContributeCard
+                num={this.state.num.num3}
+                title={this.state.title.title3}
+                text={this.state.text.text3}
+              />
+            </div>
           </div>
-          <div className="row pb-5">
-            <ContributeCard
-              num={this.state.num.num1}
-              title={this.state.title.title1}
-              text={this.state.text.text1}
-            />
-            <ContributeCard
-              num={this.state.num.num2}
-              title={this.state.title.title2}
-              text={this.state.text.text2}
-            />
-            <ContributeCard
-              num={this.state.num.num3}
-              title={this.state.title.title3}
-              text={this.state.text.text3}
-            />
+        </div>
+
+        <div className="contribute__badge py-4">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-2">
+                <ContributeBadge className="img-fluid py-5" />
+              </div>
+              <div className="col-md-9">
+                <h3 className="heading heading__secondary-2 pt-5">
+                  Earn a badge ...
+                </h3>
+                <p>
+                  We award you a loyalty badge to display on your profile for
+                  the world to honor acknowledge your efforts at helping to the
+                  society to be better. We let the our world audience of women
+                  judge, by analyzing their comments shares and impacts you are
+                  making in their lives. Women and men who are already involved
+                  in women’s empowerment actives earn a badge directly at sign
+                  up, as we review your profile.
+                </p>
+              </div>
+            </div>
+
+            <div className="row py-3">
+              <div className="col-md-4 mar-lar">
+                <h4 className="heading heading__tertiary pt-5 pb-3">Ready ?</h4>
+                <Link
+                  className="nav-link button button__black "
+                  to="/contribute"
+                >
+                  JOIN US
+                </Link>
+                <p className="contribute__badge--text">
+                  Tell Your Friends About Sistaz Share
+                </p>
+              </div>
+              {/* <div className="col-md-2"></div> */}
+              <div className="col-md-5">
+                <div className="img__last">
+                  <ContributeImg3 className="img-fluid text-center" />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
